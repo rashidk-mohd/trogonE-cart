3e9ef45434349b8d0b221f6cd3d3081b884102a8
rashidk-mohd <muhammedrashid116@gmail.com>
2024-11-14 13:43:01 +0530

    first commit

--- lib/home/widgets/product_card.dart
+++ lib/home/widgets/product_card.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard(
-      {super.key,
-      required this.brand,
-      required this.discount,
-      required this.imageUrl,
-      required this.price,
-      required this.name,
-      required this.rating});
+  const ProductCard({
+    super.key,
+    required this.brand,
+    required this.discount,
+    required this.imageUrl,
+    required this.price,
+    required this.name,
+    required this.rating,
+  });
+
   final String? imageUrl;
   final double? price;
   final String? name;
@@ -18,9 +20,14 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    double cardWidth = screenWidth * 0.45; 
+    double cardHeight = screenHeight * 0.25; 
+
     return Container(
-      width: 200,
-      height: 70,
+      width: cardWidth,
+      height: cardHeight,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         color: Colors.white,
@@ -29,50 +36,67 @@ class ProductCard extends StatelessWidget {
         children: [
           Stack(
             children: [
-              Image.network("$imageUrl"),
+              Image.network(
+                "$imageUrl",
+                width: cardWidth,
+                height: cardHeight * 0.6, 
+                fit: BoxFit.cover,
+              ),
               Positioned(
-                  top: 0,
-                  right: 1,
-                  child: Row(
-                    children: [
-                      Text(
-                        "Rating $rating",
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      const Icon(
-                        Icons.star,
+                top: 8,
+                right: 8,
+                child: Row(
+                  children: [
+                    Text(
+                      "Rating ${rating??0}",
+                      style:const TextStyle(
                         color: Colors.white,
-                      )
-                    ],
-                  ))
+                      ),
+                    ),
+                    const Icon(
+                      Icons.star,
+                      color: Colors.white,
+                      size: 16,
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
+          SizedBox(height: 8),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                "$name",
-                style: const TextStyle(color: Colors.black),
+                name??"Product",
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
               ),
               Text(
-                "$price",
-                style: const TextStyle(color: Colors.black),
+                "\$${price??0}",
+                style:const TextStyle(
+                  color: Colors.black,
+                ),
               ),
             ],
           ),
+          SizedBox(height: 4),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                "$brand",
+                brand??"XYZ",
                 style: const TextStyle(color: Colors.black),
               ),
               Text(
-                "$discount",
+                "${discount??0}% Off",
                 style: const TextStyle(color: Colors.black),
               ),
             ],
-          )
+          ),
         ],
       ),
     );
